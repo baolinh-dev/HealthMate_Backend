b83f2878503c38b0ec11f90995c73d2e8e608110
baolinhdev <baolinhdev@gmail.com>
2024-12-03 15:53:54 +0700

    delete func get all comments

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -1,13 +1,9 @@
 const express = require("express");
-const { addComment, getComments } = require("../controllers/commentController");
+const { addComment } = require("../controllers/commentController");
 const { verifyToken } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
-// Thêm bình luận
 router.post("/add", verifyToken, addComment);
-
-// Lấy danh sách bình luận cho một blog
-router.get("/:blogId", getComments);
 
 module.exports = router;
